8a0714d7923c9942554118464e82a27988b527b8
Antonio Mortari <antoniomortari09@gmail.com>
2024-05-26 23:57:29 -0300

    add cors and fix limit problem

--- src/server/index.js
+++ src/server/index.js
@@ -1,28 +1,43 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const cors = require('cors');
+const { v4 } = require('uuid');
 
 const server = express();
 
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
+server.use(express.urlencoded({ limit: 1024 * 1024 * 20, type: 'application/x-www-form-urlencoded' }));
+server.use(express.json({ limit: '100mb' }));
+server.use(cors({
+    origin: '*'
+}));
 
 server.post('/file', (req, res) => {
-    const { base64 } = req.body;
+    const { upload1, upload2 } = req.body;
 
-    const typeFile = base64.split('/')[1].split(';')[0];
+    const typeFile1 = upload1.split('/')[1].split(';')[0];
 
-    const pathToFile = path.join(__dirname, '..', '..', 'public', `${Date.now()}.${typeFile}`);
+    const pathToFile1 = path.join(__dirname, '..', '..', 'public', `${v4()}.png`);
 
-    const buffer = Buffer.from(base64.split(',')[1], 'base64');
+    const typeFile2 = upload2.split('/')[1].split(';')[0];
+    const pathToFile2 = path.join(__dirname, '..', '..', 'public', `${v4()}.png`);
 
-    fs.writeFile(pathToFile, buffer, (err) => {
-        if(err){
+    const buffer1 = Buffer.from(upload1, 'base64');
+    const buffer2 = Buffer.from(upload2, 'base64');
+
+    fs.writeFileSync(pathToFile1, buffer1, (err) => {
+        if (err) {
             return res.send('Erro');
         }
-
-        return res.send('Sucesso!');
     });
+
+    fs.writeFileSync(pathToFile2, buffer2, (err) => {
+        if (err) {
+            return res.send('Erro');
+        }
+    });
+
+    return res.send('Sucesso!');
 });
 
 module.exports = { server };
